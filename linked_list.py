e9addf0a8b18aeee0e1ad8dd183f461a0acdcc7b
yashgkar <yashgarudkar@gmail.com>
2019-11-16 01:43:29 +0530

    Day 2: Linked List

---
+++ linked_list.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def printll(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+    
+    '''def insert_betwn(self, prev, data):
+        
+        new_node = Node(data)
+
+        pr_node = self.head
+        if prev is pr_node.data:
+            print("match")
+            new_node.next = pr_node.next
+            pr_node.next = new_node
+            llist.printll()
+
+        elif prev is not pr_node.data:
+            pr_node = pr_node.next
+
+        else:
+            print("Previous node not detected")'''
+
+
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.prepend("E")
+llist.printll()
